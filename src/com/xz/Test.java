a9445e3cde315fcf06193f47173498c927a18513
funjava <ka1060254724@gmail.com>
2016-04-20 21:57:28 +0800

    初始化项目，首次提交

---
+++ src/com/xz/Test.java
@@ -0,0 +1,19 @@
+package com.xz;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("max length");
+		
+	}
+	
+	public void getRight(){
+		
+		String name="problem a";
+		System.out.println(name.lastIndexOf("d"));
+		
+	}
+
+}
